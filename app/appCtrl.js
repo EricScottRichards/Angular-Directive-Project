eeed94081daea404d8e12b55afaf275f355c424a
EricScottRichards <ericscottrichards@gmail.com>
2015-05-13 22:20:25 -0600

    aFinished Step 2 on README.md

--- app/appCtrl.js
+++ app/appCtrl.js
@@ -1,4 +1,12 @@
 var app = angular.module('directivePractice');
 app.controller('appCtrl', function($scope){
-	
+
+	$scope.user = {
+		name: 'Geoff McMammy',
+		age: 43,
+		email: 'geofdude@gmail.com'
+	}
+
+
+
 })
--- app/directives/directive.js
+++ app/directives/directive.js
@@ -2,6 +2,19 @@ var app = angular.module('directivePractice');
 app.directive('dirDisplay', function(){
 	return {
 		templateUrl: 'app/directives/directiveTmpl.html',
-		restrict: '',
+		// restrict: '',
+		link: function(scope, element, attrs){
+			scope.showToggle = true;
+			// var test;
+			// var check;
+			console.log(scope)
+			console.log(element)
+			console.log(attrs)
+			element.on('click', function(){
+  			scope.showToggle = !scope.showToggle;
+  			// console.log($scope.showToggle);
+  			scope.$apply();
+			})
+		}
 	}
 })

f5dc11fa0710dd7cc9bb0c887167fe36d4067ec0
EricScottRichards <ericscottrichards@gmail.com>
2015-05-13 22:46:02 -0600

    fixed it the issue. getting 5 printouts now.

---
+++ app/directives/dirWeather.js
@@ -0,0 +1,10 @@
+var app = angular.module('directivePractice');
+app.directive('dirWeather', function(){
+	return {
+		templateUrl: 'app/directives/dirWeather.html',
+		// restrict: '',
+		// link: function(scope, element, attrs){
+			
+		// }
+	}
+})
